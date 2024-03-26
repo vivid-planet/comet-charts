4d35ee648bdddefe0d9aec75c2a73f7d0d45394a
Franz Unger <franz.unger@vivid-planet.com>
2024-03-26 06:42:57 +0100

    Add changesets
    - Add package.json for changesets
    - Add package.json for every chart (mandatory)
    - Delete all changelogs
      They currently don't contain much and will be
      created when adding changesets
    - Add action for creating PRs when changeset was added
      Uses custom changeset:version-script which calls
      sync-versions.js which writes the updated version
      of package.json to Chart.yaml
    - After merging the automatically created PR the
      changed charts are published

---
+++ sync-versions.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+fs.readdirSync('charts').forEach(dir => {
+    const version = require(`./charts/${dir}/package.json`).version;
+
+    const filename = `./charts/${dir}/Chart.yaml`;
+    let str = fs.readFileSync(filename).toString();
+    str = str.replace(/version: (.*)/m, `version: ${version}`);
+    fs.writeFileSync(filename, str);
+});
